22691a75a7a2292e89c0c8767d5f580a9244f31e
Sami Odeh <samiodeh1337@gmail.com>
2024-07-03 14:58:12 +0300

    fixes

--- src/routes/product-details/index.jsx
+++ src/routes/product-details/index.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useMemo, useState, useEffect } from "react";
+import "./product.styles.scss";
+import { useParams } from "react-router-dom";
+import { selectCategoriesMap } from "../../store/category.selector";
+import { useSelector } from "react-redux";
+import exampleText from "../../assets/images/bodycare/pa/1.txt";
+import Markdown from "react-markdown";
 
 const ProductDetails = () => {
-  return <div>ProductDetails </div>;
+  const params = useParams();
+  const { productId, category } = params;
+  const categoriesMap = useSelector(selectCategoriesMap);
+
+  const product = useMemo(
+    () =>
+      categoriesMap
+        ? categoriesMap[category].find((elem) => elem.id == productId)
+        : null,
+    [categoriesMap, productId, category]
+  );
+
+  const { name, imageUrl, price } = product;
+
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    // Fetch the .txt file content
+    fetch(exampleText)
+      .then((response) => response.text())
+      .then((data) => setText(data))
+      .catch((error) => console.error("Error fetching the text file:", error));
+  }, []);
+
+  return (
+    <div className="container product-container">
+      <div className="product-card-container">
+        <div className="img-container">
+          <img
+            src={imageUrl}
+            alt={name}
+            loading="lazy"
+            onClick={() => navigate(`${product.id}`)}
+          />
+        </div>
+        <div className="product-footer">
+          <h3 className="name">{name}</h3>
+          <p className="price">$ {price?.toFixed(2)}</p>
+        </div>
+      </div>
+      <Markdown>{text}</Markdown>
+    </div>
+  );
 };
 
 export default ProductDetails;
