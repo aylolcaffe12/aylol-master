17464fea3fd9e3fb519524ca55e28626b28605e2
abohamad1996 <abohamad1996@users.noreply.github.com>
2024-07-03 12:53:22 +0300

    add text files

---
+++ src/SHOP_DATA.js
@@ -0,0 +1,424 @@
+import im1 from "./assets/images/perfume/1.jpg";
+import im2 from "./assets/images/perfume/2.jpg";
+
+import im3 from "./assets/images/bodycare/1.jpg";
+import im4 from "./assets/images/bodycare/2.jpg";
+import im5 from "./assets/images/bodycare/3.jpg";
+import im6 from "./assets/images/bodycare/4.jpg";
+import im7 from "./assets/images/bodycare/5.jpg";
+import im8 from "./assets/images/bodycare/6.jpg";
+import im9 from "./assets/images/bodycare/7.jpg";
+import im10 from "./assets/images/bodycare/8.jpg";
+import im11 from "./assets/images/bodycare/9.jpg";
+import im12 from "./assets/images/bodycare/10.jpg";
+import im13 from "./assets/images/bodycare/11.jpg";
+import im14 from "./assets/images/bodycare/12.jpg";
+import im15 from "./assets/images/bodycare/13.jpg";
+import im16 from "./assets/images/bodycare/14.jpg";
+import im17 from "./assets/images/bodycare/15.jpg";
+import im18 from "./assets/images/bodycare/16.jpg";
+import im19 from "./assets/images/bodycare/17.jpg";
+import im20 from "./assets/images/bodycare/18.jpg";
+
+import im21 from "./assets/images/haircare/1.jpg";
+import im22 from "./assets/images/haircare/2.jpg";
+import im23 from "./assets/images/haircare/3.jpg";
+import im24 from "./assets/images/haircare/4.jpg";
+import im25 from "./assets/images/haircare/5.jpg";
+import im26 from "./assets/images/haircare/6.jpg";
+import im27 from "./assets/images/haircare/7.jpg";
+
+import im28 from "./assets/images/skincare/1.jpg";
+import im29 from "./assets/images/skincare/2.jpg";
+import im30 from "./assets/images/skincare/3.jpg";
+import im31 from "./assets/images/skincare/4.jpg";
+import im32 from "./assets/images/skincare/5.jpg";
+import im33 from "./assets/images/skincare/6.jpg";
+import im34 from "./assets/images/skincare/7.jpg";
+import im35 from "./assets/images/skincare/8.jpg";
+import im36 from "./assets/images/skincare/9.jpg";
+import im37 from "./assets/images/skincare/10.jpg";
+import im38 from "./assets/images/skincare/11.jpg";
+import im39 from "./assets/images/skincare/12.jpg";
+import im40 from "./assets/images/skincare/13.jpg";
+import im41 from "./assets/images/skincare/14.jpg";
+import im42 from "./assets/images/skincare/15.jpg";
+import im43 from "./assets/images/skincare/16.jpg";
+import im44 from "./assets/images/skincare/17.jpg";
+
+
+
+
+
+
+
+
+
+
+export const SHOP_DATA = [
+  {
+    // skin care
+    title: "shampoos",
+    items: [
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im28,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im29,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im30,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im31,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im32,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im33,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im34,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im35,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im36,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im37,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im38,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im39,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im40,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im41,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im42,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im43,
+        des : "",
+        price: 500,
+      },
+      {
+        id: 1,
+        name: "Skin Care",
+        imageUrl: im44,
+        des : "",
+        price: 500,
+      },
+    ],
+  },
+  {
+    title: "makeups",
+    items: [
+      {
+        id: 6,
+        name: "",
+        imageUrl: im3,
+        des : "",
+        price: 45,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im4,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im5,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im6,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im7,
+        des :"",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im8,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im9,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im10,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im11,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im12,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im13,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im14,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im15,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im16,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im17,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im18,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im19,
+        des : "",
+        price: 60,
+      },
+      {
+        id: 7,
+        name: "",
+        imageUrl: im20,
+        des : "",
+        price: 60,
+      },
+
+    ],
+  },
+  {
+    title: "skincare",
+    items: [
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im21,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im22,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im23,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im24,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im25,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im26,
+        des : "",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: " ",
+        imageUrl: im27,
+        des : "",
+        price: 25,
+      },
+    ],
+  },
+  {
+    title: "perfumes",
+    items: [
+      {
+        id: 16,
+        name: "או דה פרפיום",
+        imageUrl: "https://unsplash.com/photos/xxcKsiwCUQ4/download?force=true",
+        price: 80,
+      },
+      {
+        id: 17,
+        name: "קולון",
+        imageUrl: "https://unsplash.com/photos/M8-omPx8tYs/download?force=true",
+        price: 70,
+      },
+      {
+        id: 18,
+        name: "בודי מיסט",
+        imageUrl: "https://unsplash.com/photos/nXjB6dm4uWk/download?force=true",
+        price: 40,
+      },
+      {
+        id: 19,
+        name: "בושם מוצק",
+        imageUrl: "https://unsplash.com/photos/W1pweRmEiw4/download?force=true",
+        price: 55,
+      },
+      {
+        id: 20,
+        name: "שמן בושם",
+        imageUrl: "https://unsplash.com/photos/10vqDJktZF4/download?force=true",
+        price: 65,
+      },
+    ],
+  },
+  {
+    title: "body-lotions",
+    items: [
+      {
+        id: 21,
+        name: "",
+        imageUrl: im1,
+        des: "فخامة الرائحة، تفرد الحضور، الجمال العطري وانتعاش الفواكه الاستوائية التي تسبح في سائل المسك العطر لتنتعش فاكهياً يدوم طويلاً مع مزيج مثير من وردة اليلانج وقلب حيوي من زهرة السوسن وزهرة السوسن. خشب الأرز لإحساس بالنظافة ينتشر في أرجاء المكان. المكونات العليا: الألدهيدات، الكمثرى روائح قلب العطر: المسك، الزهور، الفاكهة الاستوائية قاعدة العطر: العنبر، المسك",
+        price: 15,
+      },
+      {
+        id: 22,
+        name: "",
+        imageUrl: im2,
+        des: "قطرات حسية فاخرة من التوت الطازج في مزيج من المسك المنعش لرائحة تهدئك وتدوم معك، نفحات سكرية مفصلة من الورد ونبض الفانيليا نظيفة وقطرات خفيفة من القرفة ممزوجة لتعشقها كما لو كنت تحت تأثير العاصفة من السحر والإثارة التي تستمر لساعات. العائلة الشرقية: الأزهار المسكية المكونات العليا: الألدهيدات قلب العطر: التوت، الزهور، المسك قلب العطر: العنبر، المسك",
+        price: 35,
+      },
+    ],
+  },
+];
--- src/components/category-list/category-list.component.jsx
+++ src/components/category-list/category-list.component.jsx
@@ -3,33 +3,33 @@ import "./categories.styles.scss";
 const categories = [
   {
     id: 1,
-    title: "שמפו",
+    title: "Skin Care",
     imageUrl: "https://unsplash.com/photos/hXCo2kJpF2E/download?force=true",
     route: "shop/shampoos",
   },
   {
     id: 2,
-    title: "איפור",
+    title: "Body Care",
     imageUrl: "https://unsplash.com/photos/vyhYvCiL3QQ/download?force=true",
     route: "shop/makeups",
   },
   {
     id: 3,
-    title: "טיפוח עור",
+    title: "Hair Care",
     imageUrl:
       "https://unsplash.com/photos/7TlWZkpxCb0/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE5ODQwMzgxfA&force=true",
     route: "shop/skincare",
   },
   {
     id: 4,
-    title: "בשמים",
+    title: "Make Up",
     imageUrl:
       "https://unsplash.com/photos/j7zEby7Z7eg/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8cGVyZnVtZXN8ZW58MHx8fHwxNzE5ODIwODc1fDA&force=true",
     route: "shop/perfumes",
   },
   {
     id: 5,
-    title: "קרם גוף",
+    title: "Perfume & Musk",
     imageUrl:
       "https://unsplash.com/photos/p3O5f4u95Lo/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE5ODM5MTQzfA&force=true",
     route: "shop/body-lotions",
